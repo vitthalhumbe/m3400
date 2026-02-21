e52183a007aa85a976bebfe647ebe2175f697b10
vitthalhumbe <vitthal.3454@gmail.com>
2026-02-21 22:21:37 +0530

    Added readme.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Vitthal | Kuron",
+  title: "Vitthal Humbe",
   description: "My personal portfolio website.",
 };
 
